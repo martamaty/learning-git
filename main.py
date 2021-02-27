b808276dd9dbd1705ee44c1fcfcfa3ee252fb01e
Marta Matyjaszczyk <marta.maty.gdn@gmail.com>
2021-02-27 23:33:32 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+numbers=[]
+for i in range(0,101):
+    if i%5==0:
+        numbers.append(i)
+print(numbers)
+
+cubes=[number*number*number for number in numbers]
+print(f"liczby podniesione do potengi 3 {cubes}")
