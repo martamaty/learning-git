cf1b347803c1ca5654d4e04576fec1ad8ec0e92b
Marta Matyjaszczyk <marta.maty.gdn@gmail.com>
2021-02-28 23:48:06 +0100

    Adding1

---
+++ branch_es.py
@@ -0,0 +1,17 @@
+napis=("Co dziszaj kupuje?")
+print(napis)
+dicto={
+    "warzywniaka":["marchew","pomarancza","banany"],
+    "piekarnie":["bulki","chleb","rogal"]
+}
+
+for i in dicto:
+    print("jezeli ide do",i,"kupuje",dicto[i])
+
+count = 0
+for key, value in dicto.items(): 
+    if isinstance(value, list):
+        count += len(value) 
+
+print("w sumie to kupuje",count,"produktow")
+print("branch-es")
