0d5d6c438cbaf1504eb58cdedcb913c3e51c3b73
martamaty <79811015+martamaty@users.noreply.github.com>
2021-05-26 14:49:26 +0200

    Add files via upload

---
+++ 13.2.sqlite_w-praktyce.py
@@ -0,0 +1,99 @@
+
+# `ex_01_conection_to_db.py`
+
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+   """ create a database connection to a SQLite database """
+   conn = None
+   try:
+       conn = sqlite3.connect(db_file)
+       print(f"Connected to {db_file}, sqlite version: {sqlite3.version}")
+   except Error as e:
+       print(e)
+   finally:
+       if conn:
+           conn.close()
+
+def create_connection_in_memory():
+   """ create a database connection to a SQLite database """
+   conn = None
+   try:
+       conn = sqlite3.connect(":memory:")
+       print(f"Connected, sqlite version: {sqlite3.version}")
+   except Error as e:
+       print(e)
+   finally:
+       if conn:
+           conn.close()
+
+if __name__ == '__main__':
+   create_connection(r"database.db")
+   create_connection_in_memory()
+
+# ex_02_create_tables.py
+
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+   """ create a database connection to the SQLite database
+       specified by db_file
+   :param db_file: database file
+   :return: Connection object or None
+   """
+   conn = None
+   try:
+       conn = sqlite3.connect(db_file)
+       return conn
+   except Error as e:
+       print(e)
+
+   return conn
+
+def execute_sql(conn, sql):
+   """ Execute sql
+   :param conn: Connection object
+   :param sql: a SQL script
+   :return:
+   """
+   try:
+       c = conn.cursor()
+       c.execute(sql)
+   except Error as e:
+       print(e)
+
+if __name__ == "__main__":
+
+   create_projects_sql = """
+   -- projects table
+   CREATE TABLE IF NOT EXISTS projects (
+      id integer PRIMARY KEY,
+      nazwa text NOT NULL,
+      start_date text,
+      end_date text
+   );
+   """
+
+   create_tasks_sql = """
+   -- zadanie table
+   CREATE TABLE IF NOT EXISTS tasks (
+      id integer PRIMARY KEY,
+      projekt_id integer NOT NULL,
+      nazwa VARCHAR(250) NOT NULL,
+      opis TEXT,
+      status VARCHAR(15) NOT NULL,
+      start_date text NOT NULL,
+      end_date text NOT NULL,
+      FOREIGN KEY (projekt_id) REFERENCES projects (id)
+   );
+   """
+
+   db_file = "database.db"
+
+   conn = create_connection(db_file)
+   if conn is not None:
+       execute_sql(conn, create_projects_sql)
+       execute_sql(conn, create_tasks_sql)
+       conn.close()
